f6cb5f7f4ecfc764ead34ad1c4c401256cbee618
Momoko Iida <miida@c5r1s7.42tokyo.jp>
2026-07-13 23:03:03 +0900

    デバッグ途中：return_to_aのロジック修正中

--- medium.c
+++ medium.c
@@ -6,11 +6,12 @@
 /*   By: miida <miida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 21:03:19 by miida             #+#    #+#             */
-/*   Updated: 2026/07/12 23:11:11 by miida            ###   ########.fr       */
+/*   Updated: 2026/07/13 22:57:35 by miida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	get_approx_sqrt(int nodes)
 {
@@ -43,6 +44,67 @@ void	move_to_b(t_list **stack_a, t_list **stack_b, int min, int max)
 	}
 }
 
+int	find_target_index(t_list *stack_b, int target)
+{
+	int	index;
+
+	index = 0;
+	while (stack_b)
+	{
+		if (stack_b->rank == target)
+			return (index);
+		stack_b = stack_b->next;
+		index++;
+	}
+	return (-1);
+}
+
+void	rb_or_rrb(t_list **stack_b, int target_idx)
+{
+	int	total;
+
+	total = count_node(stack_b);
+	if (target_idx <= total / 2)
+	{
+		while (target_idx-- > 0)
+			rotate_b(stack_b);
+	}
+	else
+	{
+		while (target_idx++ < total)
+			reverse_rotate_b(stack_b);
+	}
+}
+
+void	return_to_a(t_list **stack_a, t_list **stack_b,
+					int max_rank, int chunk_size)
+{
+	int	target;
+	int	target_count;
+	int	idx;
+
+	target = max_rank;
+	target_count = max_rank - chunk_size;
+	while (target > target_count && *stack_b)
+	{
+		idx = find_target_index(*stack_b, target);
+		if (idx == -1)
+		{
+			target--;
+			continue ;
+		}
+		rb_or_rrb(stack_b, idx);
+		push_a(stack_a, stack_b);
+		if (*stack_b && (*stack_b)->rank == target - 1)
+		{
+			push_a(stack_a, stack_b);
+			swap_a(stack_a);
+			target--;
+		}
+		target--;
+	}
+}
+
 void	medium(t_list **stack_a, t_list **stack_b)
 {
 	int	chunk_size;
@@ -54,13 +116,24 @@ void	medium(t_list **stack_a, t_list **stack_b)
 	chunk_size = get_approx_sqrt(total);
 	min_rank = 0;
 	max_rank = chunk_size - 1;
+	printf("-total:%d\n-chunk_size:%d\n-min_rank:%d\n-max_rank:%d\n", total, chunk_size, min_rank, max_rank);
 	while (min_rank < total)
 	{
 		move_to_b(stack_a, stack_b, min_rank, max_rank);
+		printf("min:%d max:%d\n", min_rank, max_rank);
 		min_rank += chunk_size;
 		max_rank += chunk_size;
 	}
+	max_rank = total - 1;
+	min_rank = max_rank - chunk_size;
+	while (stack_b)
+	{
+		return_to_a(stack_a, stack_b, max_rank, chunk_size);
+		max_rank -= chunk_size;
+		min_rank -= chunk_size;
+	}
 }
+
 /*
 テスト用：push_bまでできた部分のテスト。スタックの内容を表示する関数
 stack_bに逆順でチャンクごと、ランク順で入っていることの確認
@@ -71,7 +144,7 @@ cc medium.c push_swap.c -o push_swap_test
 */
 
 //↓↓テスト用コード↓↓
-/*
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -85,7 +158,8 @@ void print_stack(t_list *stack, char *name) {
     printf("\n");
 }
 
-int main(int argc, char **argv) {
+int	main(int argc, char **argv)
+{
     t_list *stack_a = NULL;
     t_list *stack_b = NULL;
 	int i = 1;
@@ -106,10 +180,24 @@ int main(int argc, char **argv) {
     // 2. 実装した medium 関数の実行
     medium(&stack_a, &stack_b);
 
-    printf("--- After move_to_b ---\n");
+    printf("--- After Sorting ---\n");
     print_stack(stack_a, "A");
     print_stack(stack_b, "B");
 
+    // 3. ソート結果の簡易チェック
+    int is_sorted = 1;
+    t_list *tmp = stack_a;
+    while (tmp && tmp->next) {
+        if (tmp->rank > tmp->next->rank) {
+            is_sorted = 0;
+            break;
+        }
+        tmp = tmp->next;
+    }
+    if (is_sorted && stack_b == NULL)
+        printf("\n✅ Success: Stack A is sorted!\n");
+    else
+        printf("\n❌ Failure: Stack A is not sorted or Stack B is not empty.\n");
+
     return (0);
 }
-*/
